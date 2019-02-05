476cc5d055f703ec2e1f608a5eba191ab048bc7a
Haosen Wang <46936289+hxw5204@users.noreply.github.com>
2019-02-04 19:15:37 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, redirect, request, render_template, url_for
+from flask_script import Manager
+
+app = Flask(__name__)
+manager = Manager(app=app)
+app.config.from_object("settings.DevelopmentConfig")
+base_url = 'http://127.0.0.1:5000'
+
+
+@app.route('/<int:user_id>',methods=['GET','POST'])
+def home(user_id):
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        return redirect(base_url+f'/{user_id}/detail')
+
+
+@app.route('/<int:user_id>/detail', methods=['GET'])
+def detail(user_id):
+    return render_template('detail.html',info=user_id)
+
+
+if __name__ == '__main__':
+    manager.run()
