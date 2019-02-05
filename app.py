3aa7de7243f740391d648411f04a7820e08f9b78
Haosen Wang <46936289+hxw5204@users.noreply.github.com>
2019-02-05 15:16:02 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,3 @@
-"""
-Author: Haosen Wang
-2/2/2019
-"""
-
-
 from flask import Flask, redirect, request, render_template, url_for
 from flask_script import Manager
 
@@ -13,7 +7,7 @@ app.config.from_object("settings.DevelopmentConfig")
 base_url = 'http://127.0.0.1:5000'
 
 
-@app.route('/<int:user_id>',methods=['GET','POST'])
+@app.route('/<int:user_id>/',methods=['GET','POST'])
 def home(user_id):
     if request.method == 'GET':
         return render_template('home.html',user_id=user_id)
@@ -21,9 +15,30 @@ def home(user_id):
         return redirect(base_url+f'/{user_id}/detail')
 
 
-@app.route('/<int:user_id>/detail', methods=['GET'])
+@app.route('/<int:user_id>/detail/', methods=['GET'])
 def detail(user_id):
     return render_template('detail.html',info=user_id)
+
+
+@app.route('/',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        # template default store in templates file
+        return render_template('login.html')
+    else:
+        # request for header
+        # request.query_string
+
+        # request for content
+        user = request.form.get('user')
+        pwd = request.form.get('pwd')
+
+        # .....database operations
+        if user == '12' and pwd == '':
+            # jump to other pages
+            return redirect(base_url+f'/{user}/')
+        else:
+            return render_template('login.html', error='wrong user info')
 
 
 if __name__ == '__main__':
