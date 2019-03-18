fc3a5c50ea42c445043529c058c7db68b417453f
Haosen Wang <46936289+hxw5204@users.noreply.github.com>
2019-03-18 10:12:57 -0400

    Add files via upload

--- backend/__init__.py
+++ backend/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restful import Api
-from backend.main.api import Home,Detail
+from backend.main.api import User, CreditCardInfo, Transaction, CategoryRatio, CurrentTime
 from .main.database import db
 
 def create_app():
@@ -13,7 +13,10 @@ def create_app():
     app.config.from_object('backend.config.secure')
     db.init_app(app)
     api = Api(app)
-    api.add_resource(Home, '/<int:UID>/home')
-    api.add_resource(Detail, '/<int:UID>/detail')
+    api.add_resource(User,'/<int:UID>/userInfo')
+    api.add_resource(CreditCardInfo,'/<int:UID>/creditCardInfo')
+    api.add_resource(Transaction,'/<int:UID>/transactions')
+    api.add_resource(CategoryRatio,'/<int:UID>/categoryRatio')
+    api.add_resource(CurrentTime,'/currentTime')
 
     return app
--- backend/main/api.py
+++ backend/main/api.py
@@ -3,54 +3,108 @@ from flask_restful import reqparse, Resource
 from datetime import datetime
 from .model import *
 
+userInfo = reqparse.RequestParser()
+creditCardInfo = reqparse.RequestParser()
+transaction = reqparse.RequestParser()
+categoryRatio = reqparse.RequestParser()
+currentTime = reqparse.RequestParser()
 
-home = reqparse.RequestParser()
-detail = reqparse.RequestParser()
 
-
-class Home(Resource):
+# UserInfo, CreditCardInfo, Transaction, CategoryRatio, CurrentTime
+class User(Resource):
     def get(self, UID):
-        home.add_argument('year', required=False, help='transactions for particular year, default is latest year',
-                          default=2018)
-        home.add_argument('month', required=False, help='transaction for particular month, default is latest month')
+        user = existed(UID)
+        if user:
+            data = {'firstname': user[0][0],
+                    'lastname': user[0][1],
+                    'age': user[0][2],
+                    'gender': user[0][3]}
 
-        args = home.parse_args()
+            data = jsonify(data)
+            return make_response(data, 200)
+        else:
+            data = {'auth': 0, 'description': 'user is not existed'}
+            data = jsonify(data)
+            return make_response(data, 404)
+
+    def patch(self):
+        pass
+
+
+class CreditCardInfo(Resource):
+    def get(self, UID):
+        user = existed(UID)
+        if user:
+            data = get_credit_card_info(UID)
+            result = {}
+            n = 0
+            for i in data:
+                result[n] = {'card_number': i[0],
+                             'cvv': i[1],
+                             'exp_date': i[2]}
+                n += 1
+
+            result = jsonify(result)
+            return make_response(result, 200)
+        else:
+            data = {'auth': 0, 'description': 'user is not existed'}
+            data = jsonify(data)
+            return make_response(data, 404)
+
+
+class Transaction(Resource):
+    def get(self, UID):
+        transaction.add_argument('year', required=False,
+                                 help='transactions for particular year, default is latest year')
+        transaction.add_argument('month', required=False,
+                                 help='transaction for particular month, default is latest month')
+        transaction.add_argument('recent_transactions', required=False, help='look for recent transaction', default=True)
+        args = transaction.parse_args()
         year = args['year']
         month = args['month']
+        recent_transactions = args['recent_transactions']
+        user = existed(UID)
+        if user:
 
+            if not year:
+                year = int(get_latest_year(UID)[0])
 
-        if existed(UID):
-            if month:
-                data = homeget_month(UID, year, month)
+            if not month:
+                month = int(str(get_latest_month(UID, year)).split(sep='-')[1])
+
+            if recent_transactions:
+                data = get_recent_transactions(UID, year, month)
             else:
-                month = int(str(homeget_latestMonth(UID, year)).split(sep='-')[1])
-                data = homeget_month(UID, year, month)
+                data = get_transactions(UID, year, month)
 
             result = {}
             for i in data:
                 TID = i[1]
                 result[TID] = {'UID': i[0], 'date': i[2], 'time': i[3], 'company': i[4], 'category': i[5],
-                               'amount': i[6]}
+                               'amount': i[6], 'type': i[7]}
 
             result = jsonify(result)
             return make_response(result, 200)
-            # return data
+            pass
         else:
-            data = jsonify({'auth': 0, 'description': 'user not existed'})
+            data = {'auth': 0, 'description': 'user is not existed'}
+            data = jsonify(data)
             return make_response(data, 404)
 
-    def post(self, UID):
-        if existed(UID):
-            year = 2018
-            yearData = homeget(UID, year)
-            yearExpense = 0
-            month = int(str(homeget_latestMonth(UID, year)).split(sep='-')[1])
-            monthData = homeget_month(UID, year, month)
-            monthExpense = 0
-            categoryYearRatio = {}
-            recentTrans = {}
-            recentTransflag = 0
-            monthTrans = {}
+
+class CategoryRatio(Resource):
+    def get(self, UID):
+        categoryRatio.add_argument('year', required=False,
+                                   help='transactions for particular year, default is latest year')
+        categoryRatio.add_argument('month', required=False,
+                                   help='transaction for particular month, default is latest month')
+        args = categoryRatio.parse_args()
+        year = args['year']
+        month = args['month']
+        user = existed(UID)
+        if user:
+            expense = 0
+            ratio = {}
             Education = 0
             HealthCare = 0
             Apparel = 0
@@ -60,14 +114,17 @@ class Home(Resource):
             Housing = 0
             Groceries = 0
             Food = 0
-            currentTime = datetime.now()
 
-            for i in yearData:
-                if recentTransflag != 10:
-                    TID = i[1]
-                    recentTrans[TID] = {'date': i[2], 'time': i[3], 'company': i[4], 'amount': i[6]}
-                    recentTransflag += 1
-                yearExpense += i[6]
+            if not year:
+                year = int(get_latest_year(UID)[0])
+
+            if not month:
+                month = int(str(get_latest_month(UID, year)).split(sep='-')[1])
+
+            data = get_transactions(UID, year, month)
+            for i in data:
+
+                expense += i[6]
 
                 if i[5] == 'Education':
                     Education += i[6]
@@ -88,183 +145,73 @@ class Home(Resource):
                 elif i[5] == 'Food':
                     Food += i[6]
 
-            categoryYearRatio['Education'] = int(Education/yearExpense*100)
-            categoryYearRatio['HealthCare'] = int(HealthCare/yearExpense*100)
-            categoryYearRatio['Apparel'] = int(Apparel/yearExpense*100)
-            categoryYearRatio['Transportation'] = int(Transportation/yearExpense*100)
-            categoryYearRatio['Entertainment'] = int(Entertainment/yearExpense*100)
-            categoryYearRatio['Insurance'] = int(Insurance/yearExpense*100)
-            categoryYearRatio['Housing'] = int(Housing/yearExpense*100)
-            categoryYearRatio['Groceries'] = int(Groceries/yearExpense*100)
-            categoryYearRatio['Food'] = int(Food/yearExpense*100)
-
-            for i in monthData:
-                monthExpense += i[6]
-                TID = i[1]
-                monthTrans[TID] = {'UID': i[0], 'date': i[2], 'time': i[3], 'company': i[4], 'category': i[5],
-                                   'amount': i[6]}
-
-
-
-            result = {'monthTrans': monthTrans, 'yearExpense': round(yearExpense, 2), "recentTrans": recentTrans,
-                      'monthExpense': round(monthExpense, 2), 'categoryRatio': categoryYearRatio,
-                      'currentTime':currentTime}
-
-            return make_response(jsonify(result), 200)
-
-        else:
-            data = jsonify({'auth': 0, 'description': 'user not existed'})
-            return make_response(data, 404)
-
-
-class Detail(Resource):
-
-    def get(self, UID):
-        detail.add_argument('year', required=False, help='transactions for particular year, default is latest year',
-                          default=2018)
-        detail.add_argument('month', required=False, help='transaction for particular month, default is latest month')
-
-        args = detail.parse_args()
-        year = args['year']
-        month = args['month']
-
-
-
-        if existed(UID):
-            if month:
-                data = homeget_month(UID, year, month)
-            else:
-                month = int(str(homeget_latestMonth(UID, year)).split(sep='-')[1])
-                data = homeget_month(UID, year, month)
-
-            result = {}
-            for i in data:
-                TID = i[1]
-                result[TID] = {'UID': i[0], 'date': i[2], 'time': i[3], 'company': i[4], 'category': i[5],
-                               'amount': i[6]}
-
-            result = jsonify(result)
+            ratio['Education'] = int(Education / expense * 100)
+            ratio['HealthCare'] = int(HealthCare / expense * 100)
+            ratio['Apparel'] = int(Apparel / expense * 100)
+            ratio['Transportation'] = int(Transportation / expense * 100)
+            ratio['Entertainment'] = int(Entertainment / expense * 100)
+            ratio['Insurance'] = int(Insurance / expense * 100)
+            ratio['Housing'] = int(Housing / expense * 100)
+            ratio['Groceries'] = int(Groceries / expense * 100)
+            ratio['Food'] = int(Food / expense * 100)
+            result = jsonify(ratio)
             return make_response(result, 200)
-            # return data
         else:
-            data = jsonify({'auth': 0, 'description': 'user not existed'})
-            return make_response(data, 404)
-
-    def post(self, UID):
-        currentTime = datetime.now()
-        if existed(UID):
-            year = 2018
-            yearData = homeget(UID, year)
-            yearExpense = 0
-            month = int(str(homeget_latestMonth(UID, year)).split(sep='-')[1])
-            monthData = homeget_month(UID, year, month)
-            monthExpense = 0
-            categoryYearRatio = {}
-            recentTrans = {}
-            recentTransflag = 0
-            monthTrans = {}
-            Education = 0
-            HealthCare = 0
-            Apparel = 0
-            Transportation = 0
-            Entertainment = 0
-            Insurance = 0
-            Housing = 0
-            Groceries = 0
-            Food = 0
-
-            for i in yearData:
-                if recentTransflag != 10:
-                    TID = i[1]
-                    recentTrans[TID] = {'date': i[2], 'time': i[3], 'company': i[4], 'amount': i[6]}
-                    recentTransflag += 1
-                yearExpense += i[6]
-
-                if i[5] == 'Education':
-                    Education += i[6]
-                elif i[5] == 'HealthCare':
-                    HealthCare += i[6]
-                elif i[5] == 'Apparel':
-                    Apparel += i[6]
-                elif i[5] == 'Transportation':
-                    Transportation += i[6]
-                elif i[5] == 'Entertainment':
-                    Entertainment += i[6]
-                elif i[5] == 'Insurance':
-                    Insurance += i[6]
-                elif i[5] == 'Housing':
-                    Housing += i[6]
-                elif i[5] == 'Groceries':
-                    Groceries += i[6]
-                elif i[5] == 'Food':
-                    Food += i[6]
-
-            categoryYearRatio['Education'] = int(Education / yearExpense * 100)
-            categoryYearRatio['HealthCare'] = int(HealthCare / yearExpense * 100)
-            categoryYearRatio['Apparel'] = int(Apparel / yearExpense * 100)
-            categoryYearRatio['Transportation'] = int(Transportation / yearExpense * 100)
-            categoryYearRatio['Entertainment'] = int(Entertainment / yearExpense * 100)
-            categoryYearRatio['Insurance'] = int(Insurance / yearExpense * 100)
-            categoryYearRatio['Housing'] = int(Housing / yearExpense * 100)
-            categoryYearRatio['Groceries'] = int(Groceries / yearExpense * 100)
-            categoryYearRatio['Food'] = int(Food / yearExpense * 100)
-
-            for i in monthData:
-                monthExpense += i[6]
-                TID = i[1]
-                monthTrans[TID] = {'UID': i[0], 'date': i[2], 'time': i[3], 'company': i[4], 'category': i[5],
-                                   'amount': i[6]}
-
-            result = {'monthTrans': monthTrans, 'yearExpense': round(yearExpense, 2), "recentTrans": recentTrans,
-                      'monthExpense': round(monthExpense, 2), 'categoryRatio': categoryYearRatio,
-                      'currentTime':currentTime}
-
-            return make_response(jsonify(result), 200)
-
-        else:
-            data = jsonify({'auth': 0, 'description': 'user not existed'})
+            data = {'auth': 0, 'description': 'user is not existed'}
+            data = jsonify(data)
             return make_response(data, 404)
 
 
-"""
-some tools
-"""
+class CurrentTime(Resource):
+    def get(self):
+        time = datetime.now()
+        result = jsonify(time)
+        return make_response(result, 200)
 
 
 def existed(user_id):
-    query = UserInfo.query.filter_by(UID=user_id).all()
-    if query:
-        return True
-    else:
-        return False
+    query = db.session.query(UserInfo.firstname, UserInfo.lastname, UserInfo.age, UserInfo.gender).filter(
+        UserInfo.UID ==
+        user_id).all()
+    return query
 
 
-def homeget(user_id, year):
-    query = db.session.query(TransactionInfo.UID, TransactionInfo.TID, TransactionInfo.T_date, TransactionInfo.T_time,
-                             Company.company_name, Categories.categories, TransactionInfo.amount). \
-        filter(TransactionInfo.UID == user_id). \
-        filter(db.extract('year', TransactionInfo.T_date) == year). \
-        filter(TransactionInfo.campany_id == Company.company_id). \
-        filter(TransactionInfo.categories_id == Categories.categories_id). \
-        order_by(db.desc(TransactionInfo.T_date)).order_by(db.desc(TransactionInfo.T_time)).all()
+def get_credit_card_info(user_id):
+    query = db.session.query(CreditCard.card_num, CreditCard.cvv, CreditCard.exp_date).filter(CreditCard.UID == user_id) \
+        .all()
 
     return query
 
 
-def homeget_month(user_id, year, month):
+def get_transactions(user_id, year, month):
     query = db.session.query(TransactionInfo.UID, TransactionInfo.TID, TransactionInfo.T_date, TransactionInfo.T_time,
-                             Company.company_name, Categories.categories, TransactionInfo.amount). \
+                             Company.company_name, Categories.categories, TransactionInfo.amount, TransType.trans_type). \
         filter(TransactionInfo.UID == user_id). \
         filter(db.extract('year', TransactionInfo.T_date) == year). \
         filter(db.extract('month', TransactionInfo.T_date) == month). \
         filter(TransactionInfo.campany_id == Company.company_id). \
         filter(TransactionInfo.categories_id == Categories.categories_id). \
+        filter(TransactionInfo.type_id == TransType.type_id). \
         order_by(db.desc(TransactionInfo.T_date)).order_by(db.desc(TransactionInfo.T_time)).all()
 
     return query
 
 
-def homeget_latestMonth(user_id, year):
+def get_recent_transactions(user_id, year, month):
+    query = db.session.query(TransactionInfo.UID, TransactionInfo.TID, TransactionInfo.T_date, TransactionInfo.T_time,
+                             Company.company_name, Categories.categories, TransactionInfo.amount, TransType.trans_type). \
+        filter(TransactionInfo.UID == user_id). \
+        filter(db.extract('year', TransactionInfo.T_date) == year). \
+        filter(db.extract('month', TransactionInfo.T_date) == month). \
+        filter(TransactionInfo.campany_id == Company.company_id). \
+        filter(TransactionInfo.categories_id == Categories.categories_id). \
+        filter(TransactionInfo.type_id == TransType.type_id). \
+        order_by(db.desc(TransactionInfo.T_date)).order_by(db.desc(TransactionInfo.T_time)).limit(10).all()
+
+    return query
+
+
+def get_latest_month(user_id, year):
     query = db.session.query(TransactionInfo.T_date). \
         filter(TransactionInfo.UID == user_id). \
         filter(db.extract('year', TransactionInfo.T_date) == year). \
@@ -273,9 +220,9 @@ def homeget_latestMonth(user_id, year):
     return query
 
 
-def category():
-    query = db.session.query(Categories.categories).all()
+def get_latest_year(user_id):
+    query = db.session.query(db.extract('year', TransactionInfo.T_date)). \
+        filter(TransactionInfo.UID == user_id). \
+        order_by(db.desc(TransactionInfo.T_date)).first()
 
     return query
-
-
